c39d1dda1abe2b7fbc6481eb8ae488713492c73b
Chinmay <chinmay.mewari3@gmail.com>
2026-05-26 23:17:27 +0530

    LC-rotate-array [Runtime: N/A; Memory: N/A]

--- two-pointers/rotate-array.cpp
+++ two-pointers/rotate-array.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {\
-        vector<int> temp;
-        int n = nums.size();
-        k = k%n;
-        temp.insert(temp.begin(), nums.begin()+(n-k), nums.end());
-        for(int i = n-k-1; i>=0; i--){
-            nums[i+k] = nums[i];
-        }
-        for(int j=0; j<k; j++){
-            nums[j] = temp[j];
-        }
-        
+    void rotate(vector<int>& nums, int k) {
+        nums.reverse(nums.begin(), nums.begin()+(n-1-k));
+        nums.reverse(nums.begin()+(n-k), nums.end());
+        nums.reverse(nums.begin(), nums.end());    
     }    
 };
