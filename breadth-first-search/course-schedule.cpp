0503c3da6375b2ff8717cf5dc2f55f25bb29929f
Chinmay <chinmay.mewari3@gmail.com>
2026-06-01 00:10:29 +0530

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- breadth-first-search/course-schedule.cpp
+++ breadth-first-search/course-schedule.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&adj, vector<int>&visited, int i){
+    bool dfs(vector<vector<int>>&adj, vector<int>&visited, vector<int>&path, int i){
         visited[i]=1;
+        path[i]=1;
         for(int it:adj[i]){
-            if(!visited[it]) dfs(adj, visited, it);
-            else return false;
+            if(!visited[it]) {
+                if(dfs(adj, visited, path, it)) return true;
+            }
+            else if(path[it]){
+                return true;
+            }
         }
-        return true;
+        path[i]=0;
+        return false;
     }
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(n);
@@ -14,8 +20,9 @@ public:
             adj[it[1]].push_back(it[0]);
         }
         vector<int> visited(n,0);
+        vector<int> path(n,0);
         for(int i=0; i<n; i++){
-            if(!dfs(adj, visited, i)) return false;
+            if(!visited[i]) if(dfs(adj, visited, path, i)) return false;
         }
         return true;
     }
