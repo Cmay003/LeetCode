e6983f5346051827cc8fbcbb01e0e2aff15fc36b
Chinmay <chinmay.mewari3@gmail.com>
2026-05-31 16:58:49 +0530

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- depth-first-search/course-schedule.cpp
+++ depth-first-search/course-schedule.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int V=numCourses;
-        vector<vector<int>> adj(V);
+    bool dfs(vector<vector<int>>&adj, vector<int>&visited, int i){
+        visited[i]=1;
+        for(int it:adj[i]){
+            if(!visited[it]) dfs(adj, visited, it);
+            else return false;
+        }
+        return true;
+    }
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
         for(auto it:prerequisites){
-            int u=it[0], v=it[1];
-            adj[u].push_back(v);
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int> inorder(V,0);
-        for(auto it:prerequisites){
-            inorder[it[1]]++;
+        vector<int> visited(n,0);
+        for(int i=0; i<n; i++){
+            if(!dfs(adj, visited, i)) return false;
         }
-        queue<int> q;
-        for(int i=0; i<V; i++){
-            if(inorder[i]==0) q.push(i);
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int num=q.front();
-            q.pop();
-            ans.push_back(num);
-            for(int x:adj[num]){
-                inorder[x]--;
-                if(inorder[x]==0) q.push(x);
-            }
-        }
-        return ans.size()==V;
+        return true;
     }
 };
