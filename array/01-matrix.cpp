309e7547bd05dea5a4d9b284700f168f1eb66260
Chinmay <chinmay.mewari3@gmail.com>
2026-06-01 10:18:41 +0530

    LC-01-matrix [Runtime: N/A; Memory: N/A]

--- array/01-matrix.cpp
+++ array/01-matrix.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        vector<vector<int>> visited(m, vector<int>(n,0));
-        vector<vector<int>> ans(m, vector<int>(n,0));
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> visited(n, vector<int>(m,0));
+        vector<vector<int>> ans(n, vector<int>(m,0));
         queue<pair<pair<int,int>,int>> q;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(mat[i][j]==0){
                     visited[i][j]=1;
                     q.push({{i,j},0});
                 }
             }
         }
-        int drow[4]={0,-1,0,1};
-        int dcol[4]={-1,0,1,0};
+        int drow[4]={-1,0,1,0};
+        int dcol[4]={0,-1,0,1};
         while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int dis=q.front().second;
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int d=q.front().second;
+            ans[r][c]=d;
             q.pop();
-            ans[row][col]=dis;
             for(int i=0; i<4; i++){
-                int nrow=row+drow[i];
-                int ncol=col+dcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol]){
-                    visited[nrow][ncol]=1;
-                    q.push({{nrow,ncol},dis+1});
+                int row=r+drow[i];
+                int col=c+dcol[i];
+                if(row>=0 && row<n && col>=0 && col<m && !visited[row][col]){
+                    visited[row][col]=1;
+                    q.push({{row,col},d+1});
                 }
             }
         }
