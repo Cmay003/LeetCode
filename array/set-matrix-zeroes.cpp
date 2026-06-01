292176f78d1d5cad38b246c075c0cfd8acc536d1
Chinmay <chinmay.mewari3@gmail.com>
2026-06-01 22:50:17 +0530

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

--- array/set-matrix-zeroes.cpp
+++ array/set-matrix-zeroes.cpp
@@ -3,24 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        unordered_set<int> row,col;
+        int col=1;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(matrix[i][j]==0){
-                    row.insert(i);
-                    col.insert(j);
+                if(!matrix[i][j]){
+                    matrix[i][0]=0;
+                    if(j==0){
+                        col=0;
+                    }
+                    else{
+                        matrix[0][j]=0;
+                    }
                 }
             }
         }
-        for(int it:row){
-            for(int i=0; i<m; i++){
-                matrix[it][i]=0;
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(!matrix[i][0] || !matrix[0][j]){
+                    matrix[i][j]=0;
+                }
             }
         }
-        for(int it:col){
-            for(int i=0; i<n; i++){
-                matrix[i][it]=0;
-            }
+        if(!matrix[0][0]){
+            for(int i=0; i<m; i++) matrix[0][i]=0;
+        }
+        if(!col){
+            for(int i=0; i<n; i++) matrix[i][0]=0;
         }
     }
 };
