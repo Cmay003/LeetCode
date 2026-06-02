d4452e9d1655a2f4698c0d6b49df9800deb403c5
Chinmay <chinmay.mewari3@gmail.com>
2026-06-02 17:11:53 +0530

    LC-find-eventual-safe-states [Runtime: N/A; Memory: N/A]

--- topological-sort/find-eventual-safe-states.cpp
+++ topological-sort/find-eventual-safe-states.cpp
@@ -3,9 +3,9 @@ public:
     bool dfs(int node, vector<vector<int>>&adj, vector<int>&state){
         if(state[node]==1) return false;
         if(state[node]==2) return true;
-        state[node]=1;
-        for(auto it:adj[node]){
-            if(!dfs(it, adj, state)){
+        state[node]=1;    //visiting node
+        for(int it:adj[node]){
+            if(!dfs(it,adj,state)){
                 return false;
             }
         }
@@ -13,11 +13,12 @@ public:
         return true;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //dfs
         int n=graph.size();
-        vector<int> state(n,0);
-        vector<int> ans;
+        vector<int> state(n,0);//0-> not visited 1->visiting/unsafe 2->safe
+        vector<int>ans;
         for(int i=0; i<n; i++){
-            if(dfs(i, graph, state)){
+            if(dfs(i,graph,state)){
                 ans.push_back(i);
             }
         }
