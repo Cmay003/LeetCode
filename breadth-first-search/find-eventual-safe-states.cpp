315043105df585b4e7e1204de54dd0be63a3704a
Chinmay <chinmay.mewari3@gmail.com>
2026-06-02 15:29:35 +0530

    LC-find-eventual-safe-states [Runtime: N/A; Memory: N/A]

--- breadth-first-search/find-eventual-safe-states.cpp
+++ breadth-first-search/find-eventual-safe-states.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    vector<int> topo(vector<vector<int>>&adj, int V){
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V=graph.size();
+        vector<vector<int>> adjRev(V);
         vector<int> indegree(V,0);
         for(int i=0; i<V; i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
+            indegree[i]=graph[i].size();
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
             }
         }
         queue<int> q;
@@ -14,7 +17,7 @@ public:
         vector<int> ans;
         while(!q.empty()){
             int node=q.front(); q.pop();
-            for(int it:adj[node]){
+            for(int it:adjRev[node]){
                 indegree[it]--;
                 if(indegree[it]==0) q.push(it);
             }
@@ -22,15 +25,5 @@ public:
         }
         sort(ans.begin(), ans.end());
         return ans;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<vector<int>> adjRev(n);
-        for(int i=0; i<n; i++){
-            for(auto it:graph[i]){
-                adjRev[it].push_back(i);
-            }
-        }
-        return topo(adjRev, n);
     }
 };
