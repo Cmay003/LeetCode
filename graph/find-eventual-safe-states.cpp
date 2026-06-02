b41d2c0a65a117613cba6703f1a514337adfc50a
Chinmay <chinmay.mewari3@gmail.com>
2026-06-02 15:21:28 +0530

    LC-find-eventual-safe-states [Runtime: N/A; Memory: N/A]

--- graph/find-eventual-safe-states.cpp
+++ graph/find-eventual-safe-states.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V=graph.size();
-        vector<vector<int>> adj(V);
-        vector<int> inorder(V,0);
+    vector<int> topo(vector<vector<int>>&adj, int V){
+        vector<int> indegree(V,0);
         for(int i=0; i<V; i++){
-            for(int it:graph[i]){
-                adj[it].push_back(i);
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
-            inorder[i]=graph[i].size();
         }
         queue<int> q;
         for(int i=0; i<V; i++){
-            if(inorder[i]==0) q.push(i);
+            if(indegree[i]==0) q.push(i);
         }
         vector<int> ans;
         while(!q.empty()){
-            int num=q.front();
-            q.pop();
-            ans.push_back(num);
-            for(int x:adj[num]){
-                inorder[x]--;
-                if(inorder[x]==0) q.push(x);
+            int node=q.front(); q.pop();
+            for(int it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
+            ans.push_back(node);
         }
         sort(ans.begin(), ans.end());
         return ans;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<vector<int>> adjRev(n);
+        for(int i=0; i<n; i++){
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+            }
+        }
+        return topo(adjRev, n);
     }
 };
