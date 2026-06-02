1dbf8f3b38e343af1d608c662fe5490a3a574d8e
Chinmay <chinmay.mewari3@gmail.com>
2026-06-02 16:39:25 +0530

    LC-find-eventual-safe-states [Runtime: N/A; Memory: N/A]

--- graph/find-eventual-safe-states.cpp
+++ graph/find-eventual-safe-states.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    bool dfs(int node, vector<vector<int>>&adj, vector<int>&state){
+        if(state[node]==1) return false;
+        if(state[node]==2) return true;
+        state[node]=1;
+        for(auto it:adj[node]){
+            if(!dfs(it, adj, state)){
+                return false;
+            }
+        }
+        state[node]=2;
+        return true;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V=graph.size();
-        vector<vector<int>> adjRev(V);
-        vector<int> indegree(V,0);
-        queue<int> q;
-        for(int i=0; i<V; i++){
-            indegree[i]=graph[i].size();
-            for(auto it:graph[i]){
-                adjRev[it].push_back(i);
-            }
-            if(indegree[i]==0) q.push(i);
-        }
+        int n=graph.size();
+        vector<int> state(n,0);
         vector<int> ans;
-        while(!q.empty()){
-            int node=q.front(); q.pop();
-            for(int it:adjRev[node]){
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+        for(int i=0; i<n; i++){
+            if(dfs(i, graph, state)){
+                ans.push_back(i);
             }
-            ans.push_back(node);
         }
-        sort(ans.begin(), ans.end());
         return ans;
     }
 };
