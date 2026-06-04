186962e8c6ba86f8f41254e119531cfb964513fc
Chinmay <chinmay.mewari3@gmail.com>
2026-06-05 02:43:01 +0530

    LC-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int th) {
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            dist[u][v]=wt;
+            dist[v][u]=wt;
+        }
+        for(int i=0; i<n; i++){
+            dist[i][i]=0;
+        }
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+        int city=n;
+        int minCount=1e9;
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<n; j++){
+                if(dist[i][j]<=th){
+                    count++;
+                }
+            }
+            if(count<=minCount){
+                minCount=count;
+                city=i;
+            }
+        }
+        return city;
+    }
+};
