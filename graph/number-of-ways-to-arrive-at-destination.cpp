c6421f7529f98cad5fd2f4d3d0b50cd09b794483
Chinmay <chinmay.mewari3@gmail.com>
2026-06-04 12:08:18 +0530

    LC-number-of-ways-to-arrive-at-destination [Runtime: N/A; Memory: N/A]

---
+++ graph/number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:roads){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        vector<int>time(n,1e9);
+        time[0]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.push({0,0});
+        int ans=0;
+        while(!pq.empty()){
+            int nodeTime=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto it:adj[node]){
+                int adjTime=it.second;
+                int adjNode=it.first;
+                if(adjNode==n-1) ans++;
+                if(adjTime+nodeTime<time[adjNode]){
+                    time[adjNode]=adjTime+nodeTime;
+                    pq.push({adjTime+nodeTime,adjNode});
+                }
+            }
+        }
+        return ans;
+    }
+};
