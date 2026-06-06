ff987846c6c7ff15d3df38ecef56bc257a04ef3f
Chinmay <chinmay.mewari3@gmail.com>
2026-06-07 01:19:48 +0530

    LC-most-stones-removed-with-same-row-or-column [Runtime: N/A; Memory: N/A]

---
+++ union-find/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,53 @@
+class DisjointSet{
+    vector<int> parent,size;
+    public:
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n,1);
+        for(int i=0; i<n; i++){
+            parent[i]=i;
+        }
+    }
+    int findPar(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=findPar(parent[u]);
+    }
+    void unionBySize(int u, int v){
+        int parU=findPar(u);
+        int parV=findPar(v);
+        if(parU==parV) return;
+        else if(size[parU]<size[parV]){
+            parent[parU]=parV;
+            size[parV]+=size[parU];
+        }
+        else{
+            parent[parV]=parU;
+            size[parU]+=size[parV];
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        int row=0, col=0;
+        for(auto it:stones){
+            row=max(row,it[0]);
+            col=max(col,it[1]);
+        }
+        DisjointSet ds=DisjointSet(row+col+1);
+        unordered_map<int,int> mpp;
+        for(auto it:stones){
+            int nodeRow=it[0];
+            int nodeCol=it[1]+row;
+            ds.unionBySize(nodeRow,nodeCol);
+            mpp[nodeRow]=1;
+            mpp[nodeCol]=1;
+        }
+        int count=0;
+        for(auto it:mpp){
+            if(ds.findPar(it.first)==it.first) count++;
+        }
+        return n-count;
+    }
+};
