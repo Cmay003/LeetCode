3eeb9330cd341aefafaf3a229f0e80099ff46ae4
Chinmay <chinmay.mewari3@gmail.com>
2026-06-06 17:11:15 +0530

    LC-number-of-operations-to-make-network-connected [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,47 @@
+class DisjointSet{
+    vector<int> parent, size;
+    public:
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n,1);
+        for(int i=0; i<n; i++){
+            parent[i]=i;
+        }
+    }
+    int findPar(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=findPar(parent[u]);
+    }
+    void unionBySize(int u, int v){
+        int paru=findPar(u);
+        int parv=findPar(v);
+        if(paru==parv) return;
+        else if(size[paru]<size[parv]){
+            parent[u]=parv;
+            size[parv]+=size[paru];
+        }
+        else{
+            parent[v]=paru;
+            size[paru]+=size[parv];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds=DisjointSet(n);
+        int total=connections.size();
+        if(total<n-1) return -1;
+        int ans=0;
+        for(auto it:connections){
+            int u=it[0];
+            int v=it[1];
+            if(ds.findPar(u)!=ds.findPar(v)){
+                ds.unionBySize(u,v);
+                ans++;
+            }
+        }
+        return (total-ans>=0)?total-ans:-1;
+    }
+};
