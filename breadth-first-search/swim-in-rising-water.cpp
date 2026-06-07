39d63529f57f71b2e66034a81ade3eb649c3b063
Chinmay <chinmay.mewari3@gmail.com>
2026-06-08 02:49:45 +0530

    LC-swim-in-rising-water [Runtime: N/A; Memory: N/A]

--- breadth-first-search/swim-in-rising-water.cpp
+++ breadth-first-search/swim-in-rising-water.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int drow[4]={-1,0,1,0};
-    int dcol[4]={0,-1,0,1};
-    void dfs(int n, vector<vector<int>>& grid, vector<vector<int>>&visited, int i, int j, int t){
-        visited[i][j]=1;
-        for(int k=0; k<4; k++){
-            int row=drow[k]+i;
-            int col=dcol[k]+j;
-            if(row>=0 && row<n && col>=0 && col<n && !visited[row][col] && grid[row][col]<=t){
-                dfs(n, grid, visited, row, col, t);
-            }
-        }
-    }
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
-        int low=grid[0][0], high=n*n-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            vector<vector<int>> visited(n,vector<int>(n,0));
-            dfs(n, grid, visited, 0, 0, mid);
-            if(visited[n-1][n-1]) high=mid-1;
-            else low=mid+1;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        vector<vector<int>> times(n, vector<int>(n,1e9));
+        times[0][0]=grid[0][0];
+        pq.push({grid[0][0],{0,0}});
+        while(!pq.empty()){
+            int time=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;
+            pq.pop();
+            int drow[4]={-1,0,1,0};
+            int dcol[4]={0,-1,0,1};
+            for(int i=0; i<4; i++){
+                int nr=drow[i]+row;
+                int nc=dcol[i]+col;
+                if(nr>=0 && nr<n && nc>=0 && nc<n){
+                    int newTime=max(time,grid[nr][nc]);
+                    if(newTime<times[nr][nc]){
+                        times[nr][nc]=newTime;
+                        pq.push({newTime,{nr,nc}});
+                    }                    
+                }
+            }
         }
-        return low;
+        return times[n-1][n-1];
     }
 };
