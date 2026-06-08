9f920fd74fca6cec437f36c0f1e15104cba0ba0c
Chinmay <chinmay.mewari3@gmail.com>
2026-06-08 12:09:29 +0530

    LC-swim-in-rising-water [Runtime: N/A; Memory: N/A]

--- matrix/swim-in-rising-water.cpp
+++ matrix/swim-in-rising-water.cpp
@@ -1,32 +1,59 @@
+class DisjointSet{
+    public:
+    vector<int> parent, size;
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0; i<=n; i++){
+            parent[i]=i;
+        }
+    }
+    int findPar(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=findPar(parent[u]);
+    }
+    void uni(int u, int v){
+        int parU=findPar(u);
+        int parV=findPar(v);
+        if(parU==parV) return;
+        else if(size[parU]<size[parV]){
+            parent[parU]=parV;
+            size[parV]+=size[parU];
+        }
+        else{
+            parent[parV]=parU;
+            size[parU]+=size[parV];
+        }
+    }
+};
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n=grid.size();
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
-        vector<vector<int>> times(n, vector<int>(n,1e9));
-        times[0][0]=grid[0][0];
-        pq.push({grid[0][0],{0,0}});
-        while(!pq.empty()){
-            int time=pq.top().first;
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            if(time>times[row][col]) continue;
-            if(row==n-1 && col==n-1) return time;
-            pq.pop();
+        vector<pair<int,int>> pos(n*n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                pos[grid[i][j]]={i,j};
+            }
+        }
+        DisjointSet ds=DisjointSet(n*n);
+        for(int t=0; t<n*n; t++){
+            auto [r,c]=pos[t];
             int drow[4]={-1,0,1,0};
             int dcol[4]={0,-1,0,1};
             for(int i=0; i<4; i++){
-                int nr=drow[i]+row;
-                int nc=dcol[i]+col;
-                if(nr>=0 && nr<n && nc>=0 && nc<n){
-                    int newTime=max(time,grid[nr][nc]);
-                    if(newTime<times[nr][nc]){
-                        times[nr][nc]=newTime;
-                        pq.push({newTime,{nr,nc}});
-                    }                    
+                int row=drow[i]+r; 
+                int col=dcol[i]+c; 
+                if(row>=0 && row<n && col>=0 && col<n && grid[row][col]<=t){
+                    int p1=r*n+c;
+                    int p2=row*n+col;
+                    if(ds.findPar(p1)!=ds.findPar(p2)){
+                        ds.uni(p1,p2);
+                    }
                 }
             }
+            if(findPar(0)==findPar(n*n-1)) return t;
         }
-        return times[n-1][n-1];
+        return -1;
     }
 };
