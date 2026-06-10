92668ef478185b5b10b5739bdb4eeecff86231a7
Chinmay <chinmay.mewari3@gmail.com>
2026-06-10 17:56:49 +0530

    LC-house-robber-ii [Runtime: N/A; Memory: N/A]

---
+++ array/house-robber-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int helper(vector<int> nums, int idx, bool taken){
+        if(idx==0) return (!taken)?nums[0]:0;
+        if(idx<0) return 0;
+        int take=(idx==nums.size()-1)?nums[idx]+helper(nums,idx-2,true):nums[idx]+helper(nums,idx-2,false);
+        int notTake=helper(nums,idx-1,false);
+        return max(take,notTake);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        return helper(nums, n-1, false);
+    }
+};
