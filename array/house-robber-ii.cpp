e716deb09067f692a4850ee29f3f9aad3ae593a2
Chinmay <chinmay.mewari3@gmail.com>
2026-06-10 23:59:01 +0530

    LC-house-robber-ii [Runtime: N/A; Memory: N/A]

--- array/house-robber-ii.cpp
+++ array/house-robber-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int helper(vector<vector<int>> &dp, vector<int> &nums, int idx, bool taken){
-        if(idx==0) return (!taken)?nums[0]:0;
-        if(idx<0) return 0;
-        if((dp[idx][0]!=-1 && !taken)||(dp[idx][1]!=-1 && taken)){
-            if(taken) return dp[idx][1];
-            else return dp[idx][0];
+    int help(vector<int>&nums){
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1; i<n; i++){
+            int take=nums[i]+prev2;
+            int notT=prev;
+            int ans=max(take,notT);
+            prev2=prev;
+            prev=ans;
         }
-        int take=(idx==nums.size()-1 || taken)?nums[idx]+helper(dp,nums,idx-2,true):nums[idx]+helper(dp,nums,idx-2,taken);
-        int notTake=helper(dp,nums,idx-1,taken);
-        if(taken){
-            return dp[idx][1]=max(take,notTake);
-        }
-        return dp[idx][0]=max(take,notTake);
+        return prev;
     }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return dp[n-1][0]=helper(dp, nums, n-1, false);
+        vector<int> temp1,temp2;
+        for(int i=0; i<nums.size(); i++){
+            if(i!=0) temp1.push_back(nums[i]);
+            if(i!=nums.size()-1) temp2.push_back(nums[i]);
+        }
+        return max(help(temp1),help(temp2));
     }
 };
