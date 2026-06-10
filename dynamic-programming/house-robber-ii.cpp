976be818bdb7a054e94680a13e5d4caf3b94f8a7
Chinmay <chinmay.mewari3@gmail.com>
2026-06-10 23:59:59 +0530

    LC-house-robber-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber-ii.cpp
+++ dynamic-programming/house-robber-ii.cpp
@@ -14,6 +14,8 @@ public:
         return prev;
     }
     int rob(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        if(nums.size()==1) return nums[0];
         vector<int> temp1,temp2;
         for(int i=0; i<nums.size(); i++){
             if(i!=0) temp1.push_back(nums[i]);
