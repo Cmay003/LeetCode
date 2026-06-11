1e3be1fb0605945f8db87f75531faa6380b8c695
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 18:19:54 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- matrix/unique-paths-ii.cpp
+++ matrix/unique-paths-ii.cpp
@@ -12,20 +12,22 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         if(grid[0][0]==1) return 0;
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> prev(n,0);
         for(int i=0; i<m; i++){
+            vector<int> curr(n);
             for(int j=0; j<n; j++){
                 if(grid[i][j]==0){
                     if(i==0 && j==0){ 
-                        dp[i][j]=1;
+                        curr[j]=1;
                         continue;
                     }
-                    int left=(j>0)?dp[i][j-1]:0;
-                    int up=(i>0)?dp[i-1][j]:0;
-                    dp[i][j]=left+up;
+                    int left=(j>0)?curr[j-1]:0;
+                    int up=(i>0)?prev[j]:0;
+                    curr[j]=left+up;
                 }
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
