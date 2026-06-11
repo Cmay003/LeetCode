b918cf37fa8d5cd9c72dd309c3d67408e1ae1586
Chinmay <chinmay.mewari3@gmail.com>
2026-06-12 01:20:15 +0530

    LC-minimum-falling-path-sum [Runtime: N/A; Memory: N/A]

--- matrix/minimum-falling-path-sum.cpp
+++ matrix/minimum-falling-path-sum.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& matrix, int n, int row, int col, vector<vector<int>>& dp){
-        if(row==n-1) return matrix[row][col];
-        if(dp[row][col]!=1e9) return dp[row][col];
-        int left=INT_MAX, mid=INT_MAX, right=INT_MAX;
-        if(col>0) left=matrix[row][col]+helper(matrix,n,row+1,col-1,dp);
-        mid=matrix[row][col]+helper(matrix,n,row+1,col,dp);
-        if(col!=n-1) right=matrix[row][col]+helper(matrix,n,row+1,col+1,dp);
-        return dp[row][col]=min({left,mid,right});
-    }
+    // int helper(vector<vector<int>>& matrix, int n, int row, int col, vector<vector<int>>& dp){
+    //     if(row==n-1) return matrix[row][col];
+    //     if(dp[row][col]!=1e9) return dp[row][col];
+    //     int left=INT_MAX, mid=INT_MAX, right=INT_MAX;
+    //     if(col>0) left=matrix[row][col]+helper(matrix,n,row+1,col-1,dp);
+    //     mid=matrix[row][col]+helper(matrix,n,row+1,col,dp);
+    //     if(col!=n-1) right=matrix[row][col]+helper(matrix,n,row+1,col+1,dp);
+    //     return dp[row][col]=min({left,mid,right});
+    // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         if(n==1) return matrix[0][0];
-        int ans=INT_MAX;
-        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        vector<vector<int>> dp(n,vector<int>(n,0));
         for(int i=0; i<n; i++){
-            ans=min(ans,helper(matrix,n,0,i,dp));
+            dp[0][i]=matrix[0][i];
+        }
+        for(int row=1; row<n; row++){
+            for(int col=0; col<n; col++){
+                int left=(col>0)?matrix[row][col]+dp[row-1][col-1]:1e9;
+                int mid=matrix[row][col]+dp[row-1][col];
+                int right=(col!=n-1)?matrix[row][col]+dp[row-1][col+1]:1e9;
+                dp[row][col]=min({left,mid,right});
+            }
+        }
+        int ans=1e9;
+        for(int i=0;i<n;i++){
+            ans=min(ans,dp[n-1][i]);
         }
         return ans;
     }
