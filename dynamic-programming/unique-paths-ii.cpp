1139e831020e24749772daa57a7fcbb18b8440b6
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 18:16:43 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths-ii.cpp
+++ dynamic-programming/unique-paths-ii.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(m==0 && n==0) return 1;
-        if(grid[m][n]==1) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        int left=(n>0)?helper(m,n-1,grid,dp):0;
-        int up=(m>0)?helper(m-1,n,grid,dp):0;
-        return dp[m][n]=left+up;
-    }
+    // int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
+    //     if(m==0 && n==0) return 1;
+    //     if(grid[m][n]==1) return 0;
+    //     if(dp[m][n]!=-1) return dp[m][n];
+    //     int left=(n>0)?helper(m,n-1,grid,dp):0;
+    //     int up=(m>0)?helper(m-1,n,grid,dp):0;
+    //     return dp[m][n]=left+up;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         if(grid[0][0]==1) return 0;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,grid,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==0){
+                    if(i==0 && j==0){ 
+                        dp[i][j]=1;
+                        continue;
+                    }
+                    int left=(j>0)?dp[i][j-1]:0;
+                    int up=(i>0)?dp[i-1][j]:0;
+                    dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
