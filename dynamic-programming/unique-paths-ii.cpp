a178d3aac97220be4669e27cb866c1f9f325bee4
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 17:58:42 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths-ii.cpp
+++ dynamic-programming/unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(m==0 && n==0 && grid[m][n]==0) return 1;
+        if(m==0 && n==0) return 1;
         if(grid[m][n]==1) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
         int left=(n>0)?helper(m,n-1,grid,dp):0;
@@ -11,6 +11,7 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
+        if(grid[0][0]==1) return 0;
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return helper(m-1,n-1,grid,dp);
     }
