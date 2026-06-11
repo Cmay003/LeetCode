341284cab1fde022d89e476c9c8f926951680a0a
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 21:30:56 +0530

    LC-triangle [Runtime: N/A; Memory: N/A]

--- array/triangle.cpp
+++ array/triangle.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& triangle, int row, int col){
+    int helper(vector<vector<int>>& triangle, int row, int col, vector<vector<int>>&dp){
         if(row==triangle.size()-1) return triangle[row][col];
-        int left=(col<triangle[row].size())?triangle[row][col]+helper(triangle,row+1, col):INT_MAX;
-        int right=(col<triangle[row].size())?triangle[row][col]+helper(triangle,row+1, col+1):INT_MAX;
-        return min(left,right);
+        if(dp[row][col]!=1e9) return dp[row][col];
+        int left=triangle[row][col]+helper(triangle,row+1,col,dp);
+        int right=triangle[row][col]+helper(triangle,row+1,col+1,dp);
+        return dp[row][col]=min(left,right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        return helper(triangle,0,0);
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        return helper(triangle,0,0,dp);
     }
 };
