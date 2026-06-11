9b8de324e9493d1bfc3fa3aa4b90c97b82a2affd
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 17:10:45 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- math/unique-paths.cpp
+++ math/unique-paths.cpp
@@ -10,17 +10,17 @@ public:
     int uniquePaths(int m, int n) {
         vector<int> prev(n,-1);
         for(int i=0; i<m; i++){
+            vector<int> curr(n);
             for(int j=0; j<n; j++){
-                vector<int> curr=prev;
                 if(i==0 && j==0){
-                    prev[j]=1;
+                    curr[j]=1;
                     continue;
                 }
                 int up=(i>0)?prev[j]:0;
                 int left=(j>0)?curr[j-1]:0;
                 curr[j]=left+up;
             }
-            curr=prev;
+            prev=curr;
         }
         return prev[n-1];
     }
