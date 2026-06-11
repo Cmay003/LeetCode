7f49203a64b61ed1fc1781d6af56ed282bc978b6
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 16:15:05 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- dynamic-programming/unique-paths.cpp
+++ dynamic-programming/unique-paths.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int helper(int m, int n, int i, int j){
-        if(i==m && j==n) return 1;
-        int down=(i!=m)?helper(m,n,i+1,j):0;
-        int right=(j!=n)?helper(m,n,i,j+1):0;
-        return right+down;
+    int helper(int m, int n, int i, int j, vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=(i!=m-1)?helper(m,n,i+1,j,dp):0;
+        int right=(j!=n-1)?helper(m,n,i,j+1,dp):0;
+        return dp[i][j]=right+down;
     }
     int uniquePaths(int m, int n) {
-        return helper(m,n,1,1);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return helper(m,n,0,0,dp);
     }
 };
