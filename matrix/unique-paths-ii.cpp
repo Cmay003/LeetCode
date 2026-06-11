d05013a7840cfb2dad5860508f574d50c09c32ba
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 17:55:19 +0530

    LC-unique-paths-ii [Runtime: N/A; Memory: N/A]

--- matrix/unique-paths-ii.cpp
+++ matrix/unique-paths-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(m==0 && n==0) return 1;
+        if(m==0 && n==0 && grid[m][n]==0) return 1;
         if(grid[m][n]==1) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
         int left=(n>0)?helper(m,n-1,grid,dp):0;
