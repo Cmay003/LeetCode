b71fac4fd1d85ff0ffa6fb901031cac6708e8c41
Chinmay <chinmay.mewari3@gmail.com>
2026-06-12 01:02:05 +0530

    LC-minimum-falling-path-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/minimum-falling-path-sum.cpp
+++ dynamic-programming/minimum-falling-path-sum.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& matrix, int n, int row, int col){
+    int helper(vector<vector<int>>& matrix, int n, int row, int col, vector<vector<int>>& dp){
         if(row==n-1) return matrix[row][col];
+        if(dp[row][col]!=1e9) return dp[row][col];
         int left=INT_MAX, mid=INT_MAX, right=INT_MAX;
-        if(col>0) left=matrix[row][col]+helper(matrix,n,row+1,col-1);
-        mid=matrix[row][col]+helper(matrix,n,row+1,col);
-        if(col!=n-1) right=matrix[row][col]+helper(matrix,n,row+1,col+1);
-        return min({left,mid,right});
+        if(col>0) left=matrix[row][col]+helper(matrix,n,row+1,col-1,dp);
+        mid=matrix[row][col]+helper(matrix,n,row+1,col,dp);
+        if(col!=n-1) right=matrix[row][col]+helper(matrix,n,row+1,col+1,dp);
+        return dp[row][col]=min({left,mid,right});
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
-        return min({helper(matrix,n,0,0),helper(matrix,n,0,1),helper(matrix,n,0,0)});
+        if(n==1) return matrix[0][0];
+        int ans=INT_MAX;
+        vector<vector<int>> dp(n,vector<int>(n,1e9));
+        for(int i=0; i<n; i++){
+            ans=min(ans,helper(matrix,n,0,i,dp));
+        }
+        return ans;
     }
 };
