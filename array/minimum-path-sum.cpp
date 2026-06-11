e09c4632da2568f05be82835a22f97f193d62a9a
Chinmay <chinmay.mewari3@gmail.com>
2026-06-11 18:58:05 +0530

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

--- array/minimum-path-sum.cpp
+++ array/minimum-path-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int helper(int m, int n, vector<vector<int>>& grid){
+    int helper(int m, int n, vector<vector<int>>& grid, vector<vector<int>>&dp){
         if(m==0 && n==0) return grid[m][n];
-        int left=(n>0)?grid[m][n]+helper(m,n-1,grid):INT_MAX;
-        int up=(m>0)?grid[m][n]+helper(m-1,n,grid):INT_MAX;
-        return min(left, up);
+        if(dp[m][n]!=-1) return dp[m][n];
+        int left=(n>0)?grid[m][n]+helper(m,n-1,grid,dp):INT_MAX;
+        int up=(m>0)?grid[m][n]+helper(m-1,n,grid,dp):INT_MAX;
+        return dp[m][n]=min(left, up);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        return helper(m-1,n-1,grid);
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return helper(m-1,n-1,grid,dp);
     }
 };
