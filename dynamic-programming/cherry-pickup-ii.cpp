7b18a7ec20b78ce9c9444a93980452cdb3826e2f
Chinmay <chinmay.mewari3@gmail.com>
2026-06-13 12:39:55 +0530

    LC-cherry-pickup-ii [Runtime: N/A; Memory: N/A]

--- dynamic-programming/cherry-pickup-ii.cpp
+++ dynamic-programming/cherry-pickup-ii.cpp
@@ -24,13 +24,14 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<vector<vector<int>>> dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
+        vector<vector<int>> prev(c,vector<int>(c,0));
         for(int i=0; i<c; i++){
             for(int j=0; j<c; j++){
-                if(i==j) dp[r-1][i][j]=grid[r-1][j];
-                else dp[r-1][i][j]=grid[r-1][i]+grid[r-1][j];
+                if(i==j) prev[i][j]=grid[r-1][j];
+                else prev[i][j]=grid[r-1][i]+grid[r-1][j];
             }            
         }
+        vector<vector<int>> curr(c,vector<int>(c,0));
         for(int row=r-2; row>=0; row--){
             for(int col1=0; col1<c; col1++){
                 for(int col2=0; col2<c; col2++){
@@ -41,16 +42,17 @@ public:
                             if(col1==col2) move=grid[row][col1];
                             else move=grid[row][col1]+grid[row][col2];
                             if(col1+i<c && col1+i>=0 && col2+j<c && col2+j>=0){
-                                move+=dp[row+1][col1+i][col2+j];
+                                move+=prev[col1+i][col2+j];
                             } 
                             else move=-1e8;
                             maxi=max(maxi,move);
                         }            
                     }
-                    dp[row][col1][col2]=maxi;
+                    curr[col1][col2]=maxi;
                 }
-            }            
+            }
+            prev=curr;            
         }
-        return dp[0][0][c-1];
+        return prev[0][c-1];
     }
 };
