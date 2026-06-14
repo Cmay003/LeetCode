ec7dcd08bd15a0db1b1bf9c5443d000f1a5a7f68
Chinmay <chinmay.mewari3@gmail.com>
2026-06-15 02:13:36 +0530

    LC-maximum-twin-sum-of-a-linked-list [Runtime: N/A; Memory: N/A]

--- linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -10,17 +10,40 @@
  */
 class Solution {
 public:
+    //BRUTE SOLUTION
+    // int pairSum(ListNode* head) {
+    //     ListNode* temp=head;
+    //     vector<int> arr;
+    //     while(temp!=NULL){
+    //         arr.push_back(temp->val);
+    //         temp=temp->next;
+    //     }
+    //     int i=0, j=arr.size()-1, ans=0;
+    //     while(i<j){
+    //         ans=max(ans,arr[i]+arr[j]);
+    //         i++; j--;
+    //     }
+    //     return ans;
+    // }
     int pairSum(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> arr;
-        while(temp!=NULL){
-            arr.push_back(temp->val);
-            temp=temp->next;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        int i=0, j=arr.size()-1, ans=0;
-        while(i<j){
-            ans=max(ans,arr[i]+arr[j]);
-            i++; j--;
+        ListNode* prev=NULL;
+        while(slow!=NULL){
+            ListNode* temp=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=temp;
+        }
+        int ans=0;
+        while(prev!=NULL){
+            ans=max(ans,head->val+prev->val);
+            head=head->next;
+            prev=prev->next;
         }
         return ans;
     }
