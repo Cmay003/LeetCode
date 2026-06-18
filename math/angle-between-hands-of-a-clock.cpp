cf4c93d22f2fa6df92f03e91a6936f67bdd07cdc
Chinmay <chinmay.mewari3@gmail.com>
2026-06-19 00:50:12 +0530

    LC-angle-between-hands-of-a-clock [Runtime: N/A; Memory: N/A]

--- math/angle-between-hands-of-a-clock.cpp
+++ math/angle-between-hands-of-a-clock.cpp
@@ -4,7 +4,7 @@ public:
         double x=(hour%12)*30;
         double y=((minutes/5)%12)*30;
         double z=minutes*0.5;
-        double ans=(x==y)?z:360-x-y-z;
+        double ans=(x==y)?z:abs(x-y)-z;
         return ans;
     }
 };
