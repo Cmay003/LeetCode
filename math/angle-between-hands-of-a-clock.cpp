e973b07855ac94baf9fbe3f2fad2ed811da10a57
Chinmay <chinmay.mewari3@gmail.com>
2026-06-19 01:13:43 +0530

    LC-angle-between-hands-of-a-clock [Runtime: N/A; Memory: N/A]

--- math/angle-between-hands-of-a-clock.cpp
+++ math/angle-between-hands-of-a-clock.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double x=(hour%12)*30;
-        double div=(double)minutes/5;
-        double y=((div))*30;
-        double z=minutes*0.5;
+        double x=(hour%12)*30+minutes*0.5;
+        double y=(((double)minutes/5))*30;
         double ans=abs(x-y);
-        if(x>=y) ans+=z;
-        else if(x<y) ans-=z;
         return ans;
     }
 };
