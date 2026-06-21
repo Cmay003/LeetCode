59493a0e71562940ad161a21be987a426be8efe1
Chinmay <chinmay.mewari3@gmail.com>
2026-06-22 00:46:32 +0530

    LC-maximum-ice-cream-bars [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-ice-cream-bars.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        vector<int> freq(1e5+1);
+        int maxi=0;
+        for(int x:costs){
+            freq[x]++;
+            maxi=max(maxi,x);
+        }
+        int count=0;
+        for(int x=1; x<=maxi; x++){
+            int f=freq[x];
+            if(f==0) continue;
+            int buy=min(coins/x,f);
+            if(buy==0) break;
+            count+=buy;
+            coins-=buy*x;
+        }
+        for(int x:costs){
+            freq[x]=0;
+        }
+        return count;
+    }
+};
