494411c1a437cf87b18ac8d594a219b40699abfa
Chinmay <chinmay.mewari3@gmail.com>
2026-07-25 12:50:38 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- dynamic-programming/fibonacci-number.cpp
+++ dynamic-programming/fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
-    int fib(int n) {
+    int func(int n, vector<int> &dp){
         if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2, dp);
+    }
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return func(n,dp);
     }
 };
