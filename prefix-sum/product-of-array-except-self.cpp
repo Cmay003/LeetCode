a125395c9e5f2c22b64e1bf0ccd05547a5c842c7
Chinmay <chinmay.mewari3@gmail.com>
2026-08-11 00:18:33 +0530

    LC-product-of-array-except-self [Runtime: N/A; Memory: N/A]

--- prefix-sum/product-of-array-except-self.cpp
+++ prefix-sum/product-of-array-except-self.cpp
@@ -3,22 +3,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
         vector<int> ans(n);
-        int prod=1;
-        int zero=1;
-        int cnt=0;
-        for(int i:nums){
-            if(i==0){
-                zero=0;
-                cnt++;
-            }
-            else prod*=i;
-        }
+        int prefix=1;
         for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                if(cnt>1) ans[i]=0;
-                else ans[i]=prod;
-            }
-            else ans[i]=zero*prod/nums[i];
+            ans[i]=prefix;
+            prefix*=nums[i];
+        }
+        int suffix=1;
+        for(int i=n-1; i>=0; i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
         }
         return ans;
     }
