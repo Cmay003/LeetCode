94617b9cb9676bf9e109d49e4440ebb75e46e59a
Chinmay <chinmay.mewari3@gmail.com>
2026-08-13 23:32:37 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.cpp
+++ dynamic-programming/house-robber.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int helper(vector<int> &nums, int idx, vector<int> &dp){
         if(idx<0) return 0;
-        if(dp[idx]!=0) return dp[idx];
+        if(dp[idx]!=-1) return dp[idx];
         int take=nums[idx]+helper(nums, idx-2, dp);
         int notTake=helper(nums, idx-1, dp);
         return dp[idx]=max(take,notTake);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n, 0);
+        vector<int> dp(n, -1);
         return helper(nums, n-1,dp);
     }
 };
