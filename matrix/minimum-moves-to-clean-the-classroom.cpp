bf38871d4a938ad2a7bf6a8fbcf8e00803c1d469
Chinmay <chinmay.mewari3@gmail.com>
2026-09-02 00:52:35 +0530

    LC-minimum-moves-to-clean-the-classroom [Runtime: N/A; Memory: N/A]

--- matrix/minimum-moves-to-clean-the-classroom.cpp
+++ matrix/minimum-moves-to-clean-the-classroom.cpp
@@ -3,85 +3,66 @@ public:
     int minMoves(vector<string>& classroom, int energy) {
         int m = classroom.size();
         int n = classroom[0].size();
-
-        vector<vector<int>> id(m, vector<int>(n, -1));
-
-        int k = 0;
-        int sr = 0, sc = 0;
-
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (classroom[r][c] == 'S') {
-                    sr = r;
-                    sc = c;
-                } else if (classroom[r][c] == 'L') {
-                    id[r][c] = k++;
+        vector<vector<int>> litterMap(m, vector<int>(n, -1));
+        int startX = -1, startY = -1;
+        int litterCount = 0;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (classroom[i][j] == 'S') {
+                    startX = i;
+                    startY = j;
+                } else if (classroom[i][j] == 'L') {
+                    litterMap[i][j] = litterCount++;
                 }
             }
         }
-
-        if (k == 0) return 0;
-
-        int totalMask = (1 << k) - 1;
-
-        vector<vector<vector<int>>> best(
-            m, vector<vector<int>>(n, vector<int>(1 << k, -1))
-        );
-
-        struct State {
-            int r, c, mask, e, moves;
-        };
-
-        queue<State> q;
-
-        best[sr][sc][0] = energy;
-        q.push({sr, sc, 0, energy, 0});
-
-        int dr[] = {-1, 1, 0, 0};
-        int dc[] = {0, 0, -1, 1};
-
+        
+        if (litterCount == 0) return 0;
+        
+        int targetMask = (1 << litterCount) - 1;
+        queue<tuple<int, int, int, int, int>> q;
+        q.push({startX, startY, energy, 0, 0});
+        
+        vector<vector<vector<vector<bool>>>> visited(m, vector<vector<vector<bool>>>(n, vector<vector<bool>>(energy + 1, vector<bool>(1 << litterCount, false))));
+        visited[startX][startY][energy][0] = true;
+        
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        
         while (!q.empty()) {
-            State cur = q.front();
+            auto [r, c, e, mask, steps] = q.front();
             q.pop();
-
-            for (int d = 0; d < 4; d++) {
-                int nr = cur.r + dr[d];
-                int nc = cur.c + dc[d];
-
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
-                    continue;
-
-                if (classroom[nr][nc] == 'X')
-                    continue;
-
-                int ne = cur.e - 1;
-
-                if (ne < 0)
-                    continue;
-
-                int nmask = cur.mask;
-
-                if (classroom[nr][nc] == 'R') {
-                    ne = energy;
+            
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + dirs[i];
+                int nc = c + dirs[i + 1];
+                
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && classroom[nr][nc] != 'X') {
+                    int nxt_e = e - 1;
+                    int nxt_mask = mask;
+                    
+                    if (classroom[nr][nc] == 'L') {
+                        nxt_mask |= (1 << litterMap[nr][nc]);
+                    }
+                    
+                    if (nxt_mask == targetMask) {
+                        return steps + 1;
+                    }
+                    
+                    if (classroom[nr][nc] == 'R') {
+                        nxt_e = energy;
+                    }
+                    
+                    if (nxt_e == 0 && classroom[nr][nc] != 'R') continue;
+                    
+                    if (!visited[nr][nc][nxt_e][nxt_mask]) {
+                        visited[nr][nc][nxt_e][nxt_mask] = true;
+                        q.push({nr, nc, nxt_e, nxt_mask, steps + 1});
+                    }
                 }
-
-                if (classroom[nr][nc] == 'L') {
-                    nmask |= (1 << id[nr][nc]);
-                }
-
-                if (nmask == totalMask) {
-                    return cur.moves + 1;
-                }
-
-                if (ne <= best[nr][nc][nmask])
-                    continue;
-
-                best[nr][nc][nmask] = ne;
-
-                q.push({nr, nc, nmask, ne, cur.moves + 1});
             }
         }
-
+        
         return -1;
     }
 };
