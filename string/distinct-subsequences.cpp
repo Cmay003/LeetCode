cf7174a08d13113b30597c9b50a3fbb330824886
Chinmay <chinmay.mewari3@gmail.com>
2026-09-07 20:29:57 +0530

    LC-distinct-subsequences [Runtime: N/A; Memory: N/A]

--- string/distinct-subsequences.cpp
+++ string/distinct-subsequences.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int helper(string s, string t, int i, int j){
+    int helper(string s, string t, int i, int j, vector<vector<int>> &dp){
         if(j==t.size()) return 1;
-        if(i==s.size()) return 0;        
-        if(s[i]==t[j]) return helper(s,t,i+1,j)+helper(s,t,i+1,j+1);
-        return helper(s,t,i+1,j);
+        if(i==s.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]) return dp[i][j]=helper(s,t,i+1,j)+helper(s,t,i+1,j+1);
+        return dp[i][j]=helper(s,t,i+1,j);
     }
     int numDistinct(string s, string t) {
-        // int n=s.size();
-        // int m=t.size();
-        return helper(s,t,0,0);
+        int n=s.size();
+        int m=t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return helper(s,t,0,0,dp);
     }
 };
