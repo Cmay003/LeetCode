4d4881284fa8ab11f7b25122df96047208746632
Chinmay <chinmay.mewari3@gmail.com>
2026-09-13 14:14:50 +0530

    LC-unique-paths [Runtime: N/A; Memory: N/A]

--- combinatorics/unique-paths.cpp
+++ combinatorics/unique-paths.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    // int helper(int m, int n, vector<vector<int>>&dp){
-    //     if(m==0 && n==0) return 1;
-    //     if(dp[m][n]!=-1) return dp[m][n];
-    //     int up=(m!=0)?helper(m-1,n,dp):0;
-    //     int left=(n!=0)?helper(m,n-1,dp):0;
-    //     return dp[m][n]=left+up;
-    // }
+    int helper(int m, int n, vector<int>&dp){
+        if(m==0 && n==0) return 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int up=(m==0)?0:helper(m-1,n);
+        int left=(n==0)?0:helper(m,n-1);
+        return dp[m][n]=up+left;
+    }
     int uniquePaths(int m, int n) {
-        vector<int> prev(n,-1);
-        for(int i=0; i<m; i++){
-            vector<int> curr(n);
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0){
-                    curr[j]=1;
-                    continue;
-                }
-                int up=(i>0)?prev[j]:0;
-                int left=(j>0)?curr[j-1]:0;
-                curr[j]=left+up;
-            }
-            prev=curr;
-        }
-        return prev[n-1];
+        vector<int> dp(m*n, -1);
+        return helper(m-1,n-1, dp);
     }
 };
