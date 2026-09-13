1e81c23fe78209a335a07b7d56e4b8ea196aefb4
Chinmay <chinmay.mewari3@gmail.com>
2026-09-13 17:20:39 +0530

    LC-minimum-path-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/minimum-path-sum.cpp
+++ dynamic-programming/minimum-path-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>&grid, int row, int col){
+    int helper(vector<vector<int>>&grid, int row, int col, vector<vector<int>>&dp){
         if(row==0 && col==0) return grid[0][0];
-        int up=(row!=0)?helper(grid, row-1, col)+grid[row][col]:INT_MAX;
-        int left=(col!=0)?helper(grid, row, col-1)+grid[row][col]:INT_MAX;
-        return min(up,left);
+        if(dp[row][col]!=1e9) return dp[row][col];
+        int up=(row!=0)?helper(grid, row-1, col, dp)+grid[row][col]:INT_MAX;
+        int left=(col!=0)?helper(grid, row, col-1, dp)+grid[row][col]:INT_MAX;
+        return dp[row][col]=min(up,left);
     }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        return helper(grid, n-1, m-1);
+        vector<vector<int>> dp(n, vector<int>(m,1e9));
+        return helper(grid, n-1, m-1,dp);
     }
 };
